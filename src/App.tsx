9ec0bba3c5efa134006301e02701191cf8ad7f11
SleepyJoe <magnuslange@live.dk>
2026-05-04 11:54:46 +0200

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -34,71 +34,83 @@ export default function App() {
   };
 
   return (
-    <div className="bg-blue-500 text-white flex flex-col gap-4">
-      <h1 className="text-center text-2xl font-semibold">To-Do List</h1>
-      <form className="flex flex-col gap-4">
-        <label htmlFor="addBox">New Item</label>
+    <div className="w-1/2 mx-auto px-4 py-2 rounded-2xl bg-blue-500 text-white flex flex-col gap-4">
+      <h1 className="text-center text-4xl font-semibold p-4">To-Do List</h1>
+      <form className="flex flex-col gap-2">
+        <label className="font-medium text-lg" htmlFor="addBox">
+          New Item
+        </label>
         <input
           ref={inputRef}
           name="addBox"
           id="addBox"
-          className="border-2 border-amber-500"
+          className="rounded border-2 border-amber-500 px-2 py-1"
           type="text"
         />
         <button
           type="button"
           onClick={() => onClick()}
-          className="hover:cursor-pointer border-2 border-amber-500"
+          className="font-bold text-lg px-4 py-2 mt-2 mb-2 rounded-2xl bg-amber-500 hover:cursor-pointer border-2 border-amber-500  shadow"
         >
           Add
         </button>
-        <label htmlFor="searchBox">Search</label>
+        <label className="font-medium text-lg" htmlFor="searchBox">
+          Search
+        </label>
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           name="searchBox"
           id="searchBox"
-          className="border-2 border-amber-500"
+          className="rounded border-2 border-amber-500 px-2 py-1"
           type="text"
         />
       </form>
 
-      <h2>To-Do List:</h2>
-      <ul>
-        {todos.length === 0 && "No to-dos"}
-        {todos
-          .filter((todo) =>
-            todo.title.toLowerCase().includes(query.toLowerCase()),
-          )
-          .map((todo) => (
-            <li key={todo.id}>
-              <input
-                className="hover:cursor-pointer"
-                type="checkbox"
-                checked={todo.checked}
-                onChange={(e) =>
-                  setTodos((prev) =>
-                    prev.map((t) =>
-                      t.id === todo.id
-                        ? { ...t, checked: e.target.checked }
-                        : t,
-                    ),
-                  )
-                }
-              ></input>
-              {todo.title}
-              <button
-                className="bg-red-500 hover:cursor-pointer"
-                type="button"
-                onClick={() =>
-                  setTodos((prev) => prev.filter((t) => t.id !== todo.id))
-                }
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-      </ul>
+      <h2 className="font-semibold text-2xl">Filtered:</h2>
+      {todos.length === 0 ? (
+        "No to-dos"
+      ) : (
+        <ul className="flex flex-col gap-3 pl-2 pb-2">
+          {todos
+            .filter((todo) =>
+              todo.title.toLowerCase().includes(query.toLowerCase()),
+            )
+            .map((todo) => (
+              <li key={todo.id}>
+                <div className="flex items-center gap-3">
+                  {/* custom list-disc to make it centered */}
+                  <span className="h-[6px] w-[6px] shrink-0 rounded-full bg-white" />
+                  <input
+                    className="h-4 w-4 cursor-pointer accent-amber-500 bg-red-500  shadow"
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={(e) =>
+                      setTodos((prev) =>
+                        prev.map((t) =>
+                          t.id === todo.id
+                            ? { ...t, checked: e.target.checked }
+                            : t,
+                        ),
+                      )
+                    }
+                  ></input>
+                  {/* add flex-1 to let it grow automatically (fillts entire parent element) */}
+                  <span className="flex-1 text-md">{todo.title}</span>
+                  <button
+                    className="py-1 px-3 rounded-2xl bg-red-500 text-sm font-semibold cursor-pointer shadow"
+                    type="button"
+                    onClick={() =>
+                      setTodos((prev) => prev.filter((t) => t.id !== todo.id))
+                    }
+                  >
+                    Delete
+                  </button>
+                </div>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 }
